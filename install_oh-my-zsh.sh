7c84df822d727197a00fc103902519343078ec5c
Yousan_O <yousan@l2tp.org>
2016-10-11 14:26:59 +0900

    modified sh

--- install_oh-my-zsh.sh
+++ install_oh-my-zsh.sh
@@ -1,1 +1,1 @@
-$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
